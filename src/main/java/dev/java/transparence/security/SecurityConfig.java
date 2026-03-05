d63d8d0a7ed72f0ef9b126426be6bcd6e3ee0069
GustavoBatiista <gustavobatista18@hotmail.com>
2026-03-05 02:08:19 -0300

    fix: configure CORS properly for Swagger and external requests

--- src/main/java/dev/java/transparence/security/SecurityConfig.java
+++ src/main/java/dev/java/transparence/security/SecurityConfig.java
@@ -52,9 +52,18 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "PATCH",
+                "OPTIONS"
+        ));
+
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
