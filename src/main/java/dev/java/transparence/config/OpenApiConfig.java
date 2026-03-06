037f487f8fef86ba6d451e10471056dbcbf9ba09
GustavoBatiista <gustavobatista18@hotmail.com>
2026-03-06 20:26:37 -0300

    fix: configure Swagger JWT bearer authentication for secured endpoints

--- src/main/java/dev/java/transparence/config/OpenApiConfig.java
+++ src/main/java/dev/java/transparence/config/OpenApiConfig.java
@@ -2,24 +2,26 @@ package dev.java.transparence.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(
-                        new Components()
-                                .addSecuritySchemes(
-                                        "Authorization",
-                                        new SecurityScheme()
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")));
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
